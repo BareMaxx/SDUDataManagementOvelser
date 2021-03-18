8892f581a5dbc79a233aa7e5207ae7d8e7addffc
Max Hansen (Maxx0758) <maxx0758@gmail.com>
2021-03-18 14:04:16 +0100

    First task completed

--- Lektion_7/src/com/company/Main.java
+++ Lektion_7/src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
     private static Connection connection;
     private static Scanner scanner;
     public static void main(String[] args) {
-        File file = new File("secureInfo");
+        File file = new File("C:\\Users\\maxx0\\OneDrive - Syddansk Universitet\\2.Semester\\DM\\Ovelser\\SDUDataManagementOvelser\\Lektion_7\\src\\com\\company\\secureInfo");
         String password = null;
         try {
             scanner = new Scanner(file);
@@ -19,7 +19,33 @@ public class Main {
         // write your code here
         try {
             DriverManager.registerDriver(new org.postgresql.Driver());
-            connection = DriverManager.getConnection( "jdbc:postgresql://localhost:5432/DM07", "postgres", password);
+            if (password != null) {
+                connection = DriverManager.getConnection( "jdbc:postgresql://localhost:5432/DM07", "postgres", password);
+            } else {
+                System.out.println("Password is null");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        //Insert Børge with CPR 1234567891 to the users table
+        try {
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO users (name, cpr) VALUES (?, ?)");
+            statement.setString(1, "Børge");
+            statement.setString(2, "1234567891");
+            statement.execute();
+        } catch (SQLException e) {
+             e.printStackTrace();
+        }
+
+        try {
+            PreparedStatement statement2 = connection.prepareStatement("SELECT * FROM users WHERE cpr = ?");
+            statement2.setString(1, "1234567891");
+            ResultSet set = statement2.executeQuery();
+
+            while (set.next()) {
+                System.out.println(set.getString("name") + " har CPR " + set.getString("cpr"));
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
